109bae3019caaeae2151c8911b05c57c2e7fedec
Steve LeValley <slevalley@gmail.com>
2021-04-25 14:02:40 -0700

    Connect to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -10,5 +11,7 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
+
+
 
 module.exports = sequelize;
